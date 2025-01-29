ed63cab1adb17af5858934e94f014b068456dd35
Danjo Syu <>
2025-01-29 14:25:21 +0900

    診療内容ページの実装

---
+++ page-treatment.php
@@ -0,0 +1,295 @@
+<?php 
+// `page.php` をインクルードして基本レイアウトを使用
+include get_template_directory() . '/page.php';
+?>
+<main class="p-treatment-main l-main">
+  <section class="p-treatment-symptoms">
+    <div class="p-treatment-symptoms__inner l-inner">
+      <div class="p-treatment-symptoms__block">
+        <div class="p-treatment-symptoms__heading">
+          <h3 class="c-sub-heading">以下の症状の方はご相談ください</h3>
+        </div>
+        <div class="p-treatment-symptoms__radius__inner">
+          <div class="p-treatment-symptoms__radius">
+            <div class="p-treatment-symptoms__radius__img">
+              <img src="<?php echo get_template_directory_uri()?>/assets/images/treatment-symptoms-1.png" alt="">
+            </div>
+            <div class="p-treatment-symptoms__radius__desc">
+              <p>目が赤い</p>
+            </div>
+          </div>
+          <div class="p-treatment-symptoms__radius">
+            <div class="p-treatment-symptoms__radius__img">
+              <img src="<?php echo get_template_directory_uri()?>/assets/images/treatment-symptoms-2.png" alt="">
+            </div>
+            <div class="p-treatment-symptoms__radius__desc">
+              <p>かゆい</p>
+            </div>
+          </div>
+          <div class="p-treatment-symptoms__radius">
+            <div class="p-treatment-symptoms__radius__img">
+              <img src="<?php echo get_template_directory_uri()?>/assets/images/treatment-symptoms-3.png" alt="">
+            </div>
+            <div class="p-treatment-symptoms__radius__desc">
+              <p>ゴロゴロする</p>
+            </div>
+          </div>
+          <div class="p-treatment-symptoms__radius">
+            <div class="p-treatment-symptoms__radius__img">
+              <img src="<?php echo get_template_directory_uri()?>/assets/images/treatment-symptoms-4.png" alt="">
+            </div>
+            <div class="p-treatment-symptoms__radius__desc">
+              <p>乾く</p>
+            </div>
+          </div>
+          <div class="p-treatment-symptoms__radius">
+            <div class="p-treatment-symptoms__radius__img">
+              <img src="<?php echo get_template_directory_uri()?>/assets/images/treatment-symptoms-5.png" alt="">
+            </div>
+            <div class="p-treatment-symptoms__radius__desc">
+              <p>目やにが多い</p>
+            </div>
+          </div>
+          <div class="p-treatment-symptoms__radius">
+            <div class="p-treatment-symptoms__radius__img">
+              <img src="<?php echo get_template_directory_uri()?>/assets/images/treatment-symptoms-6.png" alt="">
+            </div>
+            <div class="p-treatment-symptoms__radius__desc">
+              <p>涙が多い</p>
+            </div>
+          </div>
+          <div class="p-treatment-symptoms__radius">
+            <div class="p-treatment-symptoms__radius__img">
+              <img src="<?php echo get_template_directory_uri()?>/assets/images/treatment-symptoms-7.png" alt="">
+            </div>
+            <div class="p-treatment-symptoms__radius__desc">
+              <p>ぼやける</p>
+            </div>
+          </div>
+          <div class="p-treatment-symptoms__radius">
+            <div class="p-treatment-symptoms__radius__img">
+              <img src="<?php echo get_template_directory_uri()?>/assets/images/treatment-symptoms-8.png" alt="">
+            </div>
+            <div class="p-treatment-symptoms__radius__desc">
+              <p>まぶしい</p>
+            </div>
+          </div>
+          <div class="p-treatment-symptoms__radius">
+            <div class="p-treatment-symptoms__radius__img">
+              <img src="<?php echo get_template_directory_uri()?>/assets/images/treatment-symptoms-9.png" alt="">
+            </div>
+            <div class="p-treatment-symptoms__radius__desc">
+              <p>影が見える等</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="p-treatment-symptoms-indication__block">
+        <div class="p-treatment-symptoms-indication__heading">
+          <h3 class="c-sub-heading">対応疾患</h3>
+        </div>
+        <ul class="p-treatment-symptoms-indication__list">
+          <li class="p-treatment-symptoms-indication__item">
+            <div class="p-treatment-symptoms-indication__item__img">
+              <img src="<?php echo get_template_directory_uri()?>/assets/images/check-icon.png" alt="">
+            </div>
+            <div class="p-treatment-symptoms-indication__item__name">
+              <p>白内障</p>
+            </div>
+          </li>
+          <li class="p-treatment-symptoms-indication__item">
+            <div class="p-treatment-symptoms-indication__item__img">
+              <img src="<?php echo get_template_directory_uri()?>/assets/images/check-icon.png" alt="">
+            </div>
+            <div class="p-treatment-symptoms-indication__item__name">
+              <p>緑内障</p>
+            </div>
+          </li>
+          <li class="p-treatment-symptoms-indication__item">
+            <div class="p-treatment-symptoms-indication__item__img">
+              <img src="<?php echo get_template_directory_uri()?>/assets/images/check-icon.png" alt="">
+            </div>
+            <div class="p-treatment-symptoms-indication__item__name">
+              <p>結膜炎</p>
+            </div>
+          </li>
+          <li class="p-treatment-symptoms-indication__item">
+            <div class="p-treatment-symptoms-indication__item__img">
+              <img src="<?php echo get_template_directory_uri()?>/assets/images/check-icon.png" alt="">
+            </div>
+            <div class="p-treatment-symptoms-indication__item__name">
+              <p>ドライアイ</p>
+            </div>
+          </li>
+          <li class="p-treatment-symptoms-indication__item">
+            <div class="p-treatment-symptoms-indication__item__img">
+              <img src="<?php echo get_template_directory_uri()?>/assets/images/check-icon.png" alt="">
+            </div>
+            <div class="p-treatment-symptoms-indication__item__name">
+              <p>涙目</p>
+            </div>
+          </li>
+          <li class="p-treatment-symptoms-indication__item">
+            <div class="p-treatment-symptoms-indication__item__img">
+              <img src="<?php echo get_template_directory_uri()?>/assets/images/check-icon.png" alt="">
+            </div>
+            <div class="p-treatment-symptoms-indication__item__name">
+              <p>さかまつげ</p>
+            </div>
+          </li>
+          <li class="p-treatment-symptoms-indication__item">
+            <div class="p-treatment-symptoms-indication__item__img">
+              <img src="<?php echo get_template_directory_uri()?>/assets/images/check-icon.png" alt="">
+            </div>
+            <div class="p-treatment-symptoms-indication__item__name">
+              <p>ものもらい</p>
+            </div>
+          </li>
+          <li class="p-treatment-symptoms-indication__item">
+            <div class="p-treatment-symptoms-indication__item__img">
+              <img src="<?php echo get_template_directory_uri()?>/assets/images/check-icon.png" alt="">
+            </div>
+            <div class="p-treatment-symptoms-indication__item__name">
+              <p>加齢性黄斑変性症</p>
+            </div>
+          </li>
+          <li class="p-treatment-symptoms-indication__item">
+            <div class="p-treatment-symptoms-indication__item__img">
+              <img src="<?php echo get_template_directory_uri()?>/assets/images/check-icon.png" alt="">
+            </div>
+            <div class="p-treatment-symptoms-indication__item__name">
+              <p>糖尿性網膜症</p>
+            </div>
+          </li>
+          <li class="p-treatment-symptoms-indication__item">
+            <div class="p-treatment-symptoms-indication__item__img">
+              <img src="<?php echo get_template_directory_uri()?>/assets/images/check-icon.png" alt="">
+            </div>
+            <div class="p-treatment-symptoms-indication__item__name">
+              <p>高血圧性網膜症等</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+  <section class="p-treatment-equipment l-section">
+    <div class="p-treatment-equipment__inner l-inner">
+      <div class="p-treatment-equipment__block">
+        <div class="p-treatment-equipment__heading c-heading-block">
+          <div class="p-treatment-equipment__heading__text c-page-head__heading__text__block">
+            <div class="c-page-head__heading__text__img">
+              <img src="<?php echo get_template_directory_uri()?>/assets/images/furigana-icon.png" alt="">
+            </div>
+            <p class="c-page-head__heading__text">EQUIPMENT</p>
+          </div>
+          <h2 class="c-heading">眼科訪問診療設備の例</h2>
+        </div>
+        <div class="p-treatment-equipment__body">
+          <div class="p-treatment-equipment__content">
+            <div class="p-treatment-equipment__content__name">
+              <h3 class="c-sub-heading">充電式単眼倒像検眼鏡 BS-Ⅲ LED</h3>
+            </div>
+            <div class="p-treatment-equipment__content__flex">
+              <div class="p-treatment-equipment__content__img">
+                <img src="<?php echo get_template_directory_uri()?>/assets/images/equipment-1.png" alt="">
+              </div>
+              <div class="p-treatment-equipment__content__text">
+                <p>
+                  眼底(目の奥の網膜)を照らす器械です。レンズと<br>
+                  セットで使用します。糖尿病網膜症、緑内障のス<br>
+                  クリーニング検査、散瞳検査に使用します。
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="p-treatment-equipment__content">
+            <div class="p-treatment-equipment__content__name">
+              <h3 class="c-sub-heading">眼底倒像鏡</h3>
+            </div>
+            <div class="p-treatment-equipment__content__flex">
+              <div class="p-treatment-equipment__content__img">
+                <img src="<?php echo get_template_directory_uri()?>/assets/images/equipment-2.png" alt="">
+              </div>
+              <div class="p-treatment-equipment__content__text">
+                <p>
+                  20Dと14Dのセットを使い分けて、目の奥の網<br>
+                  膜、視神経乳頭(緑内障等)などを広く照らしま<br>
+                  す。
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="p-treatment-equipment__content">
+            <div class="p-treatment-equipment__content__name">
+              <h3 class="c-sub-heading">手持ちスリットランプ</h3>
+            </div>
+            <div class="p-treatment-equipment__content__flex">
+              <div class="p-treatment-equipment__content__img">
+                <img src="<?php echo get_template_directory_uri()?>/assets/images/equipment-3.png" alt="">
+              </div>
+              <div class="p-treatment-equipment__content__text">
+                <p>
+                  眼の表面の角膜(ドライアイ等)、虹彩、水<br>
+                  晶体（白内障等）を観察する機械です。大学の一般眼<br>
+                  科診療の外来でも、椅子に座れない方に用いられ<br>
+                  ます。
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="p-treatment-equipment__content">
+            <div class="p-treatment-equipment__content__name">
+              <h3 class="c-sub-heading">眼圧計</h3>
+            </div>
+            <div class="p-treatment-equipment__content__flex">
+              <div class="p-treatment-equipment__content__img">
+                <img src="<?php echo get_template_directory_uri()?>/assets/images/equipment-4.png" alt="">
+              </div>
+              <div class="p-treatment-equipment__content__text">
+                <p>
+                  緑内障などの診療に用いる、眼圧を測定する機械<br>
+                  です。痛みは全くありません。
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="c-banner-contact l-setion">
+    <div class="c-banner-contact-inner">
+      <div class="c-banner-contact-block">
+        <div class="c-banner-contact-block__img">
+          <img src="<?php echo get_template_directory_uri()?>/assets/images/phone-icon.png" alt="">
+        </div>
+        <div class="c-banner-contact-block-body">
+          <div class="c-banner-contact-block-body__img">
+            <img src="<?php echo get_template_directory_uri()?>/assets/images/contact-field-1.png" alt="">
+          </div>
+          <div class="c-banner-contact-block-body__phoneNumber">
+            <p class="c-banner-contact-block-body__phoneNumber__desc">お問い合わせ・お申し込み</p>
+            <p class="c-banner-contact-block-body__phoneNumber__number">050-8893-2767</p>
+            <p class="c-banner-contact-block-body__phoneNumber__text">
+              当院は名古屋市内で唯一の眼科専門医による訪問診療専門クリニックです<br>
+              内科・精神科の診療も可能で、24時間365日、医師がすぐに対応。<br>
+              訪問診療で通院の負担なく、眼科と内科の診療を受けられます。
+            </p>
+            <a href="" class="c-banner-contact-block-body__email c-email-btn">
+              <div class="c-banner-contact-block-body__email__img c-email-btn-img">
+                <img src="<?php echo get_template_directory_uri()?>/assets/images/email-icon.png" alt="">
+              </div>
+              <div class="c-email-btn-text">メールで問い合わせ</div>
+            </a>
+          </div>
+          <div class="c-banner-contact-block-body-block-body__img">
+            <img src="<?php echo get_template_directory_uri()?>/assets/images/contact-field-2.png" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
+
+<?php get_footer(); ?>
