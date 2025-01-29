787f923eb367dd6dc7579c5a46e311e03fa3033b
Danjo Syu <>
2025-01-29 23:32:45 +0900

    swiperの導入

--- functions.php
+++ functions.php
@@ -7,9 +7,14 @@ function enqueue_multiple_styles() {
     get_stylesheet_directory_uri() . '/assets/css/styles.css',
     array()
   );
+  //CDNからswiper cssの読み込み
+  wp_enqueue_style(
+    'swiper-css',
+    'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css',
+    array('css-common')
+  );
 }
 add_action('wp_enqueue_scripts', 'enqueue_multiple_styles');
-
 
 //jsファイル読み込み
 function add_my_scripts() {
@@ -22,12 +27,20 @@ function add_my_scripts() {
     array(), // 依存なし
     '3.7.1', // バージョン
   );
+  wp_deregister_script('Swiper');
+  // swiper を CDN から読み込む
+  wp_enqueue_script(
+    'swiper',
+    'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js',
+    array('jquery'), // 依存なし
+    true
+  );
   
   // main.js の読み込み
   wp_enqueue_script(
     'main-script',
     get_theme_file_uri('/assets/js/main.js'),
-    array('jquery'),
+    array('swiper'),
     '1.0.0',
     true
   );
